8a0225ba281acf7d09021d6d745bb3fa556b193a
juwon5272 <jackson5272@naver.com>
2023-01-04 17:35:55 +0900

    fix: add empty components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Example from "./pages/Example";
 import Main from "./pages/Main";
 import Gallery from "./pages/Gallery";
 import Developers from "./pages/Developers";
-import Remove from "./pages/Remove";
 
 function App() {
   return (
@@ -12,8 +10,6 @@ function App() {
     >
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/example" element={<Example />} /> */}
-          <Route path="/remove" element={<Remove />} />
           <Route path="/" element={<Main />} />
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/developers" element={<Developers />} />
