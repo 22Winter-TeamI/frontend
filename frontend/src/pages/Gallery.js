671b61078d66cac8a8c17c665c2648c4146cb9c0
juwon5272 <jackson5272@naver.com>
2023-01-24 14:35:36 +0900

    feat: arrange gallery page images

--- frontend/src/pages/Gallery.js
+++ frontend/src/pages/Gallery.js
@@ -1,20 +1,82 @@
-import Banner from "../components/Banner";
-import MenuBar from "../components/MenuBar";
-// import ResultPicture from "../components/ResultPicture";
-import ResultPicture from "../components/ResultPic";
-import styles from "../css/Main.module.css";
-import Carousel from "../components/Carousel";
+import React from "react";
+// import styles from "../css/Login.module.css";
+import Nav from "../components/Nav";
+import ResultPicture from "../components/ResultPicture";
 import change1 from "../images/change1.png";
 import change3 from "../images/change3.png";
-import Slider from "../components/Slider";
+
+// function Gallery() {
+//   return (
+//     <div
+//       style={{ backgroundColor: "#F2C9C9", overflow: "auto", height: "100vh" }}
+//     >
+//       <Nav />
+//       <div className={styles.container}>
+//         <section className={styles.wrapper}>
+//           <div
+//             style={{
+//               width: "100%",
+//               display: "flex",
+//               padding: "5%",
+//               fontSize: "55px",
+//             }}
+//           >
+//             Repaint Background
+//           </div>
+//           <div
+//             style={{
+//               display: "flex",
+//               flexDirection: "columns",
+//               width: "100%",
+//               justifyContent: "center",
+//               padding: "0 10%",
+//             }}
+//           >
+//             <div className={styles.containerTest}>
+//               <ResultPicture before={change1} after={change3} />
+//               <ResultPicture before={change1} after={change3} />
+//               <ResultPicture before={change1} after={change3} />
+//               <ResultPicture before={change1} after={change3} />
+//               <ResultPicture before={change1} after={change3} />
+//               <ResultPicture before={change1} after={change3} />
+//             </div>
+//           </div>
+//         </section>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Gallery;
+
+// import Banner from "../components/Banner";
+// import MenuBar from "../components/MenuBar";
+// import ResultPicture from "../components/ResultPicture";
+import styles from "../css/Gallery.module.css";
+// import Carousel from "../components/Carousel";
+// import change1 from "../images/change1.png";
+// import change3 from "../images/change3.png";
+// import Slider from "../components/Slider";
 
 function Gallery() {
+  const img = [
+    ["../images/change1.png", "../images/change3.png"],
+    ["../images/change1.png", "../images/change3.png"],
+    ["../images/change1.png", "../images/change3.png"],
+    ["../images/change1.png", "../images/change3.png"],
+    ["../images/change1.png", "../images/change3.png"],
+    ["../images/change1.png", "../images/change3.png"],
+  ];
+  const gallerySlide = () => {
+    for (let i = 0; i < img.length; i++) {}
+  };
   return (
-    <>
-      <div className={styles.container}>
+    <div
+      style={{ backgroundColor: "#F2C9C9", overflow: "auto", height: "100vh" }}
+    >
+      <Nav />
+      <div className={styles.container} style={{ marginLeft: "20%" }}>
         <section className={styles.wrapper}>
-          <MenuBar />
-          <Carousel />
           <div
             style={{
               width: "100%",
@@ -23,8 +85,9 @@ function Gallery() {
               fontSize: "55px",
             }}
           >
-            Repaint Background
+            Gallery
           </div>
+          <div>change background</div>
           <div
             style={{
               display: "flex",
@@ -35,45 +98,34 @@ function Gallery() {
             }}
           >
             <div className={styles.containerTest}>
-              <div className={styles.navBar}>
-                <ResultPicture before={change1} after={change3} />
-              </div>
-              <div className={styles.main}>
-                <ResultPicture before={change1} after={change3} />
-              </div>
-              <div className={styles.sidebar}>
-                <ResultPicture before={change1} after={change3} />
-              </div>
-              <div className={styles.sidebar1}>
-                <ResultPicture before={change1} after={change3} />
-              </div>
-              <div className={styles.sidebar2}>
-                <ResultPicture before={change1} after={change3} />
-              </div>
-              <div className={styles.contentBox}>
-                <div className={styles.content1}>
-                  <ResultPicture before={change1} after={change3} />
-                </div>
-                <div className={styles.content2}>
-                  <ResultPicture before={change1} after={change3} />
-                </div>
-                <div className={styles.content3}>
-                  <ResultPicture before={change1} after={change3} />
-                </div>
-                <div className={styles.content3}>
-                  <ResultPicture before={change1} after={change3} />
-                </div>
-              </div>
-              <div className={styles.footer}>
-                <ResultPicture before={change1} after={change3} />
-              </div>
+              <ResultPicture before={change1} after={change3} />
+              <ResultPicture before={change1} after={change3} />
+              <ResultPicture before={change1} after={change3} />
+              <ResultPicture before={change1} after={change3} />
+              <ResultPicture before={change1} after={change3} />
+              <ResultPicture before={change1} after={change3} />
             </div>
-            {/* <Slider /> */}
+          </div>
+          <div>change background</div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "columns",
+              width: "100%",
+              justifyContent: "center",
+              padding: "0 10%",
+            }}
+          >
+            <div className={styles.containerTest}>
+              <ResultPicture before={change1} after={change3} />
+              <ResultPicture before={change1} after={change3} />
+              <ResultPicture before={change1} after={change3} />
+              <ResultPicture before={change1} after={change3} />
+            </div>
           </div>
         </section>
       </div>
-      {/* <Slider /> */}
-    </>
+    </div>
   );
 }
 
