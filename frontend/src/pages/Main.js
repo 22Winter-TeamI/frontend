06f0d6118b0173f39adf3034bf2771eda461f109
juwon5272 <jackson5272@naver.com>
2023-02-02 22:58:07 +0900

    fix : fix styles and axios

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,16 +1,9 @@
 import MenuBar from "../components/MenuBar";
 import styles from "../css/Main.module.css";
-import Carousel from "../components/Carousel";
-import Dog from "../images/Dog.jpg";
 import Dog2 from "../images/Dog2.jpg";
-import Slider from "../components/Slider";
 import Banner from "../components/Banner";
 import ResultPicture from "../components/ResultPicture";
-import change1 from "../images/change1.png";
-import change3 from "../images/change3.png";
 import ex1 from "../images/MainEx/ex1.jpeg";
-import exA from "../images/MainEx/exA.png";
-import exB from "../images/MainEx/exB.png";
 import ex3 from "../images/MainEx/ex3.jpeg";
 import ex5 from "../images/MainEx/ex5.jpg";
 import ex6 from "../images/MainEx/ex6.jpg";
@@ -20,6 +13,21 @@ import ex9 from "../images/MainEx/ex9.jpg";
 import ex10 from "../images/MainEx/ex10.jpg";
 import ex12 from "../images/MainEx/ex12.jpg";
 import ex13 from "../images/MainEx/ex13.jpg";
+import exA from "../images/MainEx/exA.png";
+import exC from "../images/MainEx/exC.png";
+import exE from "../images/MainEx/exE.png";
+import exF from "../images/MainEx/exF.png";
+import exG from "../images/MainEx/exG.png";
+import exH from "../images/MainEx/exH.png";
+import exI from "../images/MainEx/exI.png";
+import exJ from "../images/MainEx/exJ.png";
+import exL from "../images/MainEx/exL.png";
+import exM from "../images/MainEx/exM.png";
+import cam1 from "../images/Main/cam1.jpg";
+import cam2 from "../images/Main/cam2.png";
+import cam3 from "../images/Main/cam3.png";
+import cam4 from "../images/Main/cam4.png";
+import aa from "../images/MainEx/aa.png";
 
 // import Frame19 from "../images/Frame19.png";
 
@@ -51,21 +59,29 @@ function Main() {
             >
               Change your image to New Painting Style !
             </div>
-            <div style={{ display: "flex", justifyContent: "center" }}>
+            <div
+              className={styles.outWrapper}
+              // style={{ display: "flex", justifyContent: "center" }}
+            >
               <div className={styles.exWrapper}>
-                <img className={styles.exImg} src={Dog} alt="예시 이미지" />
+                <img className={styles.exImg} src={cam1} alt="예시 이미지" />
                 <div className={styles.exFont}>Original</div>
               </div>
               <div className={styles.exWrapper}>
-                <img className={styles.exImg} src={Dog2} alt="예시 이미지" />
+                <img
+                  style={{ border: "0.5px solid grey" }}
+                  className={styles.exImg}
+                  src={cam2}
+                  alt="예시 이미지"
+                />
                 <div className={styles.exFont}>Remove Background</div>
               </div>
               <div className={styles.exWrapper}>
-                <img className={styles.exImg} src={Dog} alt="예시 이미지" />
+                <img className={styles.exImg} src={cam3} alt="예시 이미지" />
                 <div className={styles.exFont}>New Background</div>
               </div>
               <div className={styles.exWrapper}>
-                <img className={styles.exImg} src={Dog2} alt="예시 이미지" />
+                <img className={styles.exImg} src={cam4} alt="예시 이미지" />
                 <div className={styles.exFont}>New Painting Style</div>
               </div>
             </div>
@@ -92,36 +108,36 @@ function Main() {
             >
               <div className={styles.containerTest}>
                 <div className={styles.navBar}>
-                  <ResultPicture before={ex8} after={exA} />
+                  <ResultPicture before={ex8} after={exH} />
                 </div>
                 <div className={styles.main}>
-                  <ResultPicture before={ex3} after={exA} />
+                  <ResultPicture before={ex3} after={exC} />
                 </div>
                 <div className={styles.sidebar}>
-                  <ResultPicture before={ex12} after={exA} />
+                  <ResultPicture before={ex12} after={exL} />
                 </div>
                 <div className={styles.sidebar1}>
-                  <ResultPicture before={ex9} after={exB} />
+                  <ResultPicture before={ex9} after={exI} />
                 </div>
                 <div className={styles.sidebar2}>
-                  <ResultPicture before={ex13} after={exB} />
+                  <ResultPicture before={ex13} after={exM} />
                 </div>
                 <div className={styles.contentBox}>
                   <div className={styles.content1}>
-                    <ResultPicture before={ex7} after={change3} />
+                    <ResultPicture before={ex7} after={exG} />
                   </div>
                   <div className={styles.content2}>
-                    <ResultPicture before={ex10} after={change3} />
+                    <ResultPicture before={ex10} after={exJ} />
                   </div>
                   <div className={styles.content3}>
-                    <ResultPicture before={ex5} after={change3} />
+                    <ResultPicture before={ex5} after={exE} />
                   </div>
                   <div className={styles.content3}>
-                    <ResultPicture before={ex1} after={change3} />
+                    <ResultPicture before={ex1} after={exA} />
                   </div>
                 </div>
                 <div className={styles.footer}>
-                  <ResultPicture before={ex6} after={change3} />
+                  <ResultPicture before={ex6} after={aa} />
                 </div>
               </div>
               {/* <Slider /> */}
